39612a44b5d39cfaa54aab1a906dd076f8cbd3a4
José Lázaro <contato.jlazaro@gmail.com>
2024-01-17 22:17:52 -0300

    feat: create blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Blog = () => {
+
+    return (
+        <main className={'flex flex-col gap-8'}>
+           blog
+        </main>
+    )
+}
+
+export default Blog
