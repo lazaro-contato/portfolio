4c29b26df24ae15f2528b0cdcd7deabbdcc7eb8c
José Lázaro <contato.jlazaro@gmail.com>
2024-01-17 22:17:41 -0300

    feat: create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const About = () => {
+
+    return (
+        <main className={'flex flex-col gap-8'}>
+            about
+        </main>
+    )
+}
+
+export default About
