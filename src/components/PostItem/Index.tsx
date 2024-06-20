e15ec610b2797bc7fb4a5206c8a4df1a34c09739
José Lázaro <contato.jlazaro@gmail.com>
2024-06-19 21:53:20 -0300

    feat: add url redirect in article item

--- src/components/PostItem/Index.tsx
+++ src/components/PostItem/Index.tsx
@@ -13,7 +13,7 @@ const PostItem = ({ article }: PostItemProps): JSX.Element => {
         'px-4 pt-1 rounded hover:bg-slate-50 transition ease-in duration-300 border-b '
       }
     >
-      <a className={'cursor-pointer'}>
+      <a className={'cursor-pointer'} href={article?.url} target={'_blank'}>
         <div className={'h-16 flex flex-row justify-between items-center'}>
           <div className={'flex flex-col gap-2 text-sm md:text-base'}>
             <span>{article?.title}</span>
@@ -21,7 +21,6 @@ const PostItem = ({ article }: PostItemProps): JSX.Element => {
               {article?.published_at && formatDate(article.published_at)}
             </span>
           </div>
-
           <div>
             <span className={'text-sm md:text-base text-grayLight'}>
               {article?.page_views_count} views
