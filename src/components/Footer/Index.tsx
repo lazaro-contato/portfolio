ed59811d98f4dd8a4bf664a82865a186c3e6188f
José Lázaro <contato.jlazaro@gmail.com>
2024-07-07 19:51:28 -0300

    fix: get current year

--- src/components/Footer/Index.tsx
+++ src/components/Footer/Index.tsx
@@ -1,11 +1,13 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear()
+
   return (
     <footer className={'flex flex-col gap-2 px-5 justify-end mb-6'}>
       <div className={'w-full h-[1px] bg-graySoft'} />
       <div className={'flex flex-col gap-2'}>
         <span className={'text-sm'}>
           {' '}
-          Copyright © 2019-2024 José Lázaro · All Rights Reserved.
+          Copyright © 2019-{currentYear} José Lázaro · All Rights Reserved.
         </span>
         <address
           className={'flex  flex-row gap-2 text-grayLight text-sm not-italic'}
