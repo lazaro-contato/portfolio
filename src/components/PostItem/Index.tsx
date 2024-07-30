e66e495f58134d3226e52d8461d6771d660e23fe
José Lázaro <contato.jlazaro@gmail.com>
2024-07-29 21:46:31 -0300

    fix: remove content when screen is small

--- src/components/PostItem/Index.tsx
+++ src/components/PostItem/Index.tsx
@@ -1,12 +1,18 @@
+'use client'
+
 import React from 'react'
 import { Article } from '@/services/http/repositories/articles/models'
 import { formatDate } from '@/utils/string'
+import { useWindowSize } from '@/utils/hooks'
 
 interface PostItemProps {
   article?: Article
 }
 
 const PostItem = ({ article }: PostItemProps): JSX.Element => {
+  const { width } = useWindowSize()
+  const isMd = !!width && width >= 640
+
   return (
     <li
       className={
@@ -15,17 +21,21 @@ const PostItem = ({ article }: PostItemProps): JSX.Element => {
     >
       <a className={'cursor-pointer'} href={article?.url} target={'_blank'}>
         <div className={'h-16 flex flex-row justify-between items-center'}>
-          <div className={'flex flex-col gap-2 text-sm md:text-base'}>
+          <div className={'flex flex-col gap-2 text-sm md:text-base w-[80%]'}>
             <span>{article?.title}</span>
-            <span className={'text-[12px] md:text-sm text-grayLight'}>
-              {article?.published_at && formatDate(article.published_at)}
-            </span>
+            {isMd && (
+              <span>
+                {article?.published_at && formatDate(article.published_at)}
+              </span>
+            )}
           </div>
-          <div>
-            <span className={'text-sm md:text-base text-grayLight'}>
-              {article?.page_views_count} views
-            </span>
-          </div>
+          {isMd && (
+            <div>
+              <span className={'text-[12px] md:text-base text-grayLight'}>
+                {article?.page_views_count} views
+              </span>
+            </div>
+          )}
         </div>
       </a>
     </li>
